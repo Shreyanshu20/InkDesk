11d73ce49e257fd2b6aff0e0b4357b944a2e0796
Mr. Well Known <shreyanshudhawale2@gmail.com>
2025-06-12 20:25:33 +0530

    base admim url

--- backend/index.js
+++ backend/index.js
@@ -14,6 +14,7 @@ const getAllowedOrigins = () => {
     'http://localhost:5173',
     'http://localhost:5174/admin',  // Admin base URL
     'https://inkdesk-frontend.onrender.com',
+    'https://inkdesk-admin.onrender.com',
     'https://inkdesk-admin.onrender.com/admin',  // Admin base URL (this is what we need)
     'https://inkdesk-backend.onrender.com'
   ];
