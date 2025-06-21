e48d256d3715932872281a54b6fc940c53bead49
Mr. Well Known <shreyanshudhawale2@gmail.com>
2025-06-21 15:51:59 +0530

    resolved build failed for admin frontend

--- admin/src/Components/Reviews/Reviews.jsx
+++ admin/src/Components/Reviews/Reviews.jsx
@@ -5,7 +5,7 @@ import Table from "../Common/Table";
 import Pagination from "../Common/Pagination";
 import BulkActions from "../Common/BulkActions";
 import { getReviewsTableConfig } from "../Common/tableConfig";
-import { useAdmin } from '../../context/AdminContext';
+import { useAdmin } from '../../Context/AdminContext';
 
 const API_BASE_URL =
   import.meta.env.VITE_BACKEND_URL || "http://localhost:5000";
